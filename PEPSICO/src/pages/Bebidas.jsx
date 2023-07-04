e8a54c3c302c84d2ee9edff917549ba13764394b
Goraz23 <angomezs.2303@gmail.com>
2023-07-04 04:47:35 -0500

    Se sube Bebidas Andrea

--- PEPSICO/src/pages/Bebidas.jsx
+++ PEPSICO/src/pages/Bebidas.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import Card from "../components/card";
 import Navbar2 from "../components/Navbar2";
+import CardProductos from "../components/CardProductos";
+import Footer from "../components/footer";
 import 'boxicons'
 
 
@@ -8,10 +10,58 @@ export default function Bebidas (){
   return(
     <div className="">
       <Navbar2></Navbar2>
-      <div className="bg-fondo bg-blue-500 p-15 flex flex-col items-center justify-center gap-16 rounded-2xl h-[30rem] max-w-6xl m-auto">
-        <h1>PEPSICO</h1>
-        <button className="bg-gray-100"><Link to ={"/nuevoslanzamientos"}>Nuevos lanzamientos</Link></button>
+      <div className="max-w-6xl m-auto">
+        <h1 className="text-2xl font-semibold text-start pt-8">
+          Las mejores bebidas para tu negocio
+        </h1>
+        <div className="grid grid-cols-3 gap-8 mt-4">
+          <CardProductos
+            nombre="Caja de Pepsi-Cola de 235ml de 8 unidades"
+            precio={108}
+            imagen={
+              "https://images.unsplash.com/photo-1546695259-ad30ff3fd643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1931&q=80"
+            }
+          />
+          <CardProductos
+            nombre="Gatorade sabor Ponche De Frutas de 1 litro cada uno. 6 Pack"
+            precio={145.80}
+            imagen={
+              "https://m.media-amazon.com/images/I/51YQoO5Q6VL.jpg"
+            }
+          />
+          <CardProductos
+            nombre="Manzanita Sol, Refresco con Jugo de Manzana, Pet de 400 mililitros. Paquete de 12, 4900 grams, 4800 mililitro"
+            precio={100}
+            imagen={
+              "https://m.media-amazon.com/images/I/81ZH+TW9jAL._AC_UF894,1000_QL80_.jpg"
+            }
+          />
+          <CardProductos
+            nombre="7UP, Refresco de Lima-Limón, 12 Pack Pet, 400ml."
+            precio={90}
+            imagen={
+              "https://m.media-amazon.com/images/I/71zGJj0OlLL._AC_UF894,1000_QL80_.jpg"
+            }
+          />
+          <CardProductos
+            nombre="Pepsi Regular, Refresco De Sabor Cola, Pet De 500 Mililitros. Paquete De 12"
+            precio={130}
+            imagen={
+              "https://m.media-amazon.com/images/I/61kqRkk5tuL._AC_SX679_.jpg"
+            }
+          />
+          <CardProductos
+            nombre="Refresco Pepsi Mix 24 pzas de 400 ml c/u a precio de socio | Sam's Club en línea"
+            precio={190}
+            imagen={
+              "https://assets.sams.com.mx/image/upload/f_auto,q_auto:eco,w_350,c_scale,dpr_auto/mx/images/product-images/img_medium/980007627m.jpg"
+            }
+          />
+        </div>
+
       </div>
+      <br/><br/><br/>
+            <Footer></Footer>
     </div>
   );
 }
